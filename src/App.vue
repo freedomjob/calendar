d7f1c9f00d5c82bd47351736e73704001707e628
flyfox11 <banjuanshutong@126.com>
2019-05-19 20:48:41 +0800

    '组件初始化'

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Calendar/>
+  </div>
+</template>
+
+<script>
+import Calendar from '@/components/Calendar'
+export default {
+  name: 'App',
+  components: {
+    Calendar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Calendar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="calendar">calendar</div>
+</template>
+
+<script>
+export default {
+  name: 'calendar',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+  .calendar {
+    font-size: 28px;
+    color: #333;
+  }
+</style>
